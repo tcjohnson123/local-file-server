adb8e3e4ea3adc4039f9bab2a6f3dec7ee083f93
tcjohnson123 <56758042+tcjohnson123@users.noreply.github.com>
2021-01-12 15:01:36 +0000

    Add UDP support for linux

--- FileServer/linux/Socket.cpp
+++ FileServer/linux/Socket.cpp
@@ -6,13 +6,48 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-class SocketImpl
+class net::EndPointImpl
+{
+public:
+	struct sockaddr_in sa;
+};
+
+net::EndPoint::EndPoint()
+	: EndPoint("0.0.0.0", 0)
+{
+}
+
+net::EndPoint::EndPoint(const char* ip, int port)
+{
+    pImpl->sa.sin_family = AF_INET;
+    pImpl->sa.sin_addr.s_addr = inet_addr(ip);
+    pImpl->sa.sin_port = htons(port);
+}
+
+net::EndPoint::~EndPoint()
+{
+}
+
+std::string net::EndPoint::ipAddress() const
+{
+	char* strAddress = inet_ntoa(pImpl->sa.sin_addr);
+	return strAddress;
+}
+
+int net::EndPoint::port() const
+{
+	return ntohs(pImpl->sa.sin_port);
+}
+	
+//////////
+
+class net::SocketImpl
 {
 public:
     int s;
 };
 
-Socket::Socket(Protocol proto) : pImpl(std::make_shared<SocketImpl>())
+net::Socket::Socket(Protocol proto) : pImpl(std::make_shared<SocketImpl>())
 {
     if (proto == Protocol::TCP)
         pImpl->s = socket(AF_INET, SOCK_STREAM, 0);
@@ -20,42 +55,37 @@ Socket::Socket(Protocol proto) : pImpl(std::make_shared<SocketImpl>())
         pImpl->s = -1;
 }
 
-bool Socket::valid() const
+bool net::Socket::valid() const
 {
     return pImpl->s != -1;
 }
 
-bool Socket::startUp()
+bool net::Socket::startUp()
 {
     return true;
 }
 
-void Socket::shutDown()
+void net::Socket::shutDown()
 {
 }
 
-bool Socket::bind(const char* ip, int port)
+bool net::Socket::bind(const EndPoint& endPoint)
 {
-    struct sockaddr_in sa;
-    sa.sin_family = AF_INET;
-    sa.sin_addr.s_addr = inet_addr(ip);
-    sa.sin_port = htons(port);
-
     // Ensure port is released to OS as soon as application closes
     int one = 1;
     setsockopt(pImpl->s, SOL_SOCKET, SO_REUSEADDR, (char*)&one, sizeof(one));
 
-    auto result = ::bind(pImpl->s, (struct sockaddr*)&sa, sizeof(sa));
+    auto result = ::bind(pImpl->s, (struct sockaddr*)&endPoint.pImpl->sa, sizeof(endPoint.pImpl->sa));
     return result != -1;
 }
 
-bool Socket::listen()
+bool net::Socket::listen()
 {
     auto result = ::listen(pImpl->s, SOMAXCONN);
     return result != -1;
 }
 
-Socket Socket::accept(std::string* clientIP)
+net::Socket net::Socket::accept(EndPoint* clientIP)
 {
     struct sockaddr_in clientaddr;
     socklen_t clientaddr_size = sizeof(clientaddr);
@@ -63,25 +93,24 @@ Socket Socket::accept(std::string* clientIP)
     client.pImpl->s = ::accept(pImpl->s, (struct sockaddr *)&clientaddr, &clientaddr_size);
     if (valid() && clientIP != nullptr)
     {
-        char* strAddress = inet_ntoa(clientaddr.sin_addr);
-        *clientIP = strAddress;
+        clientIP->pImpl->sa = clientaddr;
     }
     return client;
 }
 
-bool Socket::close()
+bool net::Socket::close()
 {
     auto result = ::close(pImpl->s);
     return result != -1;
 }
 
-int Socket::send(const char* buf, int len)
+int net::Socket::send(const char* buf, int len)
 {
     int flags = 0;
     return ::send(pImpl->s, buf, len, flags);
 }
 
-int Socket::receive(char* buf, int len)
+int net::Socket::receive(char* buf, int len)
 {
     int flags = 0;
     return ::recv(pImpl->s, buf, len, flags);
